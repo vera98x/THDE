00846ff81931e4e02e264d29cbe0ff9ff04fd744
florianhumblot <florian.humblot@student.hu.nl>
2018-10-31 15:25:06 +0100

    Msg class can now be created using a string and be serialized.

--- IR/msg.hpp
+++ IR/msg.hpp
@@ -1,8 +1,8 @@
 #ifndef _MSG_HPP
 #define _MSG_HPP
-
+#include "hwlib.hpp"
 class msg {
-    
+
 public:
     enum class CMD : uint8_t { 
         R_PLAYER_NAME, //0 
@@ -19,12 +19,38 @@ public:
         N_NOTINITIALISED
 
     };
-    
-    
-    CMD command = CMD::N_NOTINITIALISED; 
-    char naam[15] = ""; 
-    uint8_t waarde = 0;    
-    
+
+
+    CMD command = CMD::N_NOTINITIALISED;
+    char naam[15] = "";
+    uint8_t waarde = 0;
+
+    msg(){}
+    msg(hwlib::string<0> & s){
+        int colon = s.find(':');
+        int comma = s.find(',');
+        int semicolon = s.find(';');
+
+        command = static_cast<CMD>(s[colon+1] - '0');
+        if(command == CMD::R_PLAYER_NAME || command == CMD::R_KILLED_BY){
+            for(uint8_t i = comma + 1, j = 0; i < semicolon; i++, j++){
+                naam[j] = s[i];
+            }
+        }else{
+            waarde = static_cast<uint8_t>(s[comma+1] - '0');
+        }
+    }
+
+    void serialize(hwlib::string<0> & s){
+        s.clear();
+        s << "CMD:" << ((uint8_t)command + '0');
+        if(command == CMD::R_PLAYER_NAME || command == CMD::R_KILLED_BY){
+            s << naam;
+        }else{
+            s << (waarde + '0');
+        }
+        s << ';';
+    }
 };
 
 #endif 
