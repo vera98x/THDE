793e11a053aff7fe7dff93063f530a9101349c29
florianhumblot <florian.humblot@student.hu.nl>
2018-11-01 10:22:21 +0100

    Fixed initializer list causing build errors in WiFiTaak.hpp

--- IR/WifiTaak.hpp
+++ IR/WifiTaak.hpp
@@ -60,8 +60,14 @@ private:
 	}
 
 public:
-	WifiTaak(UARTLib::HardwareUART &ESP, commandListener * cl = nullptr) : task(4, "WiFi Taak"), cl(cl), state(STATE::WAITING),
-										   cmdChannelOut(this, "cmdChannelIn (WiFiTaak)"), wifi_chip(ESP), waiting_timeout(this, "WifiTaak waiting timeout") {}
+	WifiTaak(UARTLib::HardwareUART &ESP, commandListener * cl = nullptr) :
+		task(4, "WiFi Taak"),
+		cmdChannelOut(this, "cmdChannelIn (WiFiTaak)"),
+		wifi_chip(ESP),
+		waiting_timeout(this, "WifiTaak waiting timeout"),
+		cl(cl),
+		state(STATE::WAITING)
+	    {}
 
 	void commandReceived(const msg & m) {
 		cmdChannelOut.write(m);
